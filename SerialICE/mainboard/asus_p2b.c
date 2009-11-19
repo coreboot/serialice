e695950437788af3265137fdb3c3a8385a490cef
Stefan Reinauer <stepan@openbios.org>
2009-11-19 09:23:30 +0000

    From Mark Marshall <mark.marshall@csr.com>: Support for the ASUS P2B motherboard.
    git-svn-id: svn://svn.coreboot.org/serialice/trunk@46 8d503c2a-b2ab-11de-a960-3b3a9759a4e4

---
+++ SerialICE/mainboard/asus_p2b.c
@@ -0,0 +1,45 @@
+/*
+ * SerialICE 
+ *
+ * Copyright (C) 2009 Mark Marshall <mark.marshall@csr.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/*
+ * This is a chipset init file for the ASUS P2B mainboard.
+ */
+
+#define PNP_PORT                  0x3f0
+
+static void superio_init(void)
+{
+	/* Enter the configuration state. */
+	pnp_enter_ext_func_mode(PNP_PORT);
+
+	/* COMA */
+	pnp_set_logical_device(PNP_PORT, 2);
+	pnp_set_enable(PNP_PORT, 0);
+	pnp_set_iobase0(PNP_PORT, CONFIG_SERIAL_PORT);
+	pnp_set_irq0(PNP_PORT, 4);
+	pnp_set_enable(PNP_PORT, 1);
+
+	/* Exit the configuration state. */
+	pnp_exit_ext_func_mode(PNP_PORT);
+}
+
+static void chipset_init(void)
+{
+	superio_init();
+}
