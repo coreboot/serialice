783255e507926299e2f0423276990a47dc5f9672
Myles Watson <mylesgw@gmail.com>
2010-07-02 14:39:51 +0000

    emulators.diff from 2010/06/21
    Signed-off-by: Myles Watson <mylesgw@gmail.com>
    
    
    
    git-svn-id: svn://svn.coreboot.org/serialice/trunk@99 8d503c2a-b2ab-11de-a960-3b3a9759a4e4

---
+++ SerialICE/mainboard/amd_serengeti-cheetah.c
@@ -0,0 +1,44 @@
+/*
+ * SerialICE 
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2010 Arne Georg Gleditsch <arne.gleditsch@numascale.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+const char boardname[33]="AMD SERENGETI CHEETAH           ";
+
+#define SUPERIO_CONFIG_PORT	0x2e
+#define W83627HF_SP1            2
+
+static void superio_init(void)
+{
+	int i;
+	pnp_enter_ext_func_mode(SUPERIO_CONFIG_PORT);
+
+	/* Enable the serial port. */
+	pnp_set_logical_device(SUPERIO_CONFIG_PORT, W83627HF_SP1); /* COM1 */
+	pnp_set_enable(SUPERIO_CONFIG_PORT, 0);
+	pnp_set_iobase0(SUPERIO_CONFIG_PORT, 0x3f8);
+	pnp_set_irq0(SUPERIO_CONFIG_PORT, 4);
+	pnp_set_enable(SUPERIO_CONFIG_PORT, 1);
+
+	pnp_exit_ext_func_mode(SUPERIO_CONFIG_PORT);
+}
+
+static void chipset_init(void)
+{
+	superio_init();
+}
---
+++ SerialICE/mainboard/qemu-x86.c
@@ -0,0 +1,35 @@
+/*
+ * SerialICE
+ *
+ * Copyright (C) 2010 Myles Watson <mylesgw@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+const char boardname[33]="QEMU X86                        ";
+
+static void southbridge_init(void)
+{
+}
+
+static void superio_init(void)
+{
+}
+
+static void chipset_init(void)
+{
+	southbridge_init();
+	superio_init();
+}
+
