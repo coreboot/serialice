56a6c3ec4d1e6b1f99ae7a0d9f2a7df5767b9377
Idwer Vollering <vidwer@gmail.com>
2011-11-06 01:05:40 +0100

    Add SerialICE support for ASUS P4P800-VM.
    Change-Id: I0ce304c7be28f24c5612603720ae470353a4a3cb
    Signed-off-by: Idwer Vollering <vidwer@gmail.com>
    Reviewed-on: http://review.coreboot.org/418
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

---
+++ SerialICE/mainboard/asus_p4p800-vm.c
@@ -0,0 +1,57 @@
+/*
+ * SerialICE
+ *
+ * Copyright (C) 2011 Idwer Vollering <vidwer@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+const char boardname[33]="ASUS P4P800-VM                  ";
+
+#define SUPERIO_CONFIG_PORT 0x2e
+
+/* Hardware specific functions */
+static void mainboard_set_ich5(void)
+{
+	/* COM_DEC */ /* COMA at 0x3f8, COMB at 0x3f8 */
+	pci_write_config8(PCI_ADDR(0, 0x1f, 0, 0xe0), 0x0);
+	/* LPC_EN */ /* FDD_LPC_EN=1, KBC_LPC_EN=1, CNF1_LPC_EN=1 */
+	pci_write_config16(PCI_ADDR(0, 0x1f, 0, 0xe6), 0x1408);
+	/* FB_DEC_EN1 */ /* FN_F8_EN=1, decode two 512 kilobyte flash ranges */
+	pci_write_config8(PCI_ADDR(0, 0x1f, 0, 0xe3), 0x80);
+	/* FB_DEC_EN2 */ /* don't decode two 1 megabyte ranges */
+	pci_write_config8(PCI_ADDR(0, 0x1f, 0, 0xf0), 0x0);
+	/* FUNC_DIS */ /* D31_F6_DISABLE=1 (AC97 modem) */
+	pci_write_config16(PCI_ADDR(0, 0x1f, 0, 0xf2), 0x0040);
+}
+
+/* Winbond W83627THF */
+static void superio_init(void)
+{
+	pnp_enter_ext_func_mode(SUPERIO_CONFIG_PORT);
+	/* Set the clock to 48MHz */
+	pnp_write_register(SUPERIO_CONFIG_PORT, 0x24, 0x42);
+	pnp_set_logical_device(SUPERIO_CONFIG_PORT, 2);
+	pnp_set_enable(SUPERIO_CONFIG_PORT, 0);
+	pnp_set_iobase0(SUPERIO_CONFIG_PORT, 0x3f8);
+	pnp_set_irq0(SUPERIO_CONFIG_PORT, 4);
+	pnp_set_enable(SUPERIO_CONFIG_PORT, 1);
+	pnp_exit_ext_func_mode(SUPERIO_CONFIG_PORT);
+}
+
+static void chipset_init(void)
+{
+	mainboard_set_ich5();
+	superio_init();
+}
