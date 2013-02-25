40e3541f6d10108191b0bd24631125e6005ffd70
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-02-25 14:53:13 +0200

    Add Intel i845 filter
    Catch access to RCOMP bar registers on i845 northbridge.
    
    Change-Id: If73fb787da491b7c22327debc6034dbc30d6e43f
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: http://review.coreboot.org/2505
    Tested-by: build bot (Jenkins)
    Reviewed-by: Anton Kochkov <anton.kochkov@gmail.com>

--- SerialICE/simba/asus_p4p800_vm.lua
+++ SerialICE/simba/asus_p4p800_vm.lua
@@ -72,11 +72,13 @@ filter_mainboard = {
 }
 
 dofile("i82801.lua")
+dofile("intel_bars.lua")
 
 function do_mainboard_setup()
 	do_default_setup()
 
 	enable_hook_i82801dx()
+	northbridge_i845()
 
 	-- Apply mainboard hooks last, so they are the first ones to check
 	enable_hook(io_hooks, filter_mainboard)
