2cbc13e8d14aabb4523a3a0e4439d8a25839110c
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-03-16 13:11:38 +0200

    Fix generation of shared LUA library.
    Include library dependencies '-lm -ldl' in the generation
    of the shared LUA library already, in case user has to install
    patched LUA from source.
    
    Change-Id: I90e46828ea72862f7d06002df230dd6f348437eb
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: http://review.coreboot.org/2771
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- qemu-0.15.x/build.sh
+++ qemu-0.15.x/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./configure --extra-ldflags="-ldl" --disable-kvm --disable-sdl --enable-serialice \
+./configure --disable-kvm --disable-sdl --enable-serialice \
 	    --target-list="x86_64-softmmu, i386-softmmu"
 
 make
