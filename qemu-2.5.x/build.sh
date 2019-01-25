3f25390661f3cc608e1aa479296670832934790c
Felix Held <felix-github@felixheld.de>
2019-01-25 16:55:44 +0100

    qemu-2.5.x/build.sh: don't hard-code python2 path
    Change-Id: Ib98a181bb20d71ac54282b66b369d160156953dd
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/31091
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Idwer Vollering <vidwer@gmail.com>

--- qemu-2.5.x/build.sh
+++ qemu-2.5.x/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ./configure --disable-kvm --disable-sdl --enable-serialice \
 	    --target-list="x86_64-softmmu, i386-softmmu" \
-            --python=/bin/python2 --disable-gtk
+            --python=$(which python2) --disable-gtk
 
 if [ $? -eq 0 ]; then
    make
