4046fd980e183b16a1b9ded877e4cba20eb340de
Felix Singer <felixsinger@posteo.net>
2020-01-22 00:47:25 +0100

    qemu-0.15.x: Fix compile errors
    Fix "undefined reference to `gnutls_kx_set_priority'" by disabling VNC
    TLS support and enforce to use Python 2.
    
    Change-Id: I14fc8429de409aa9588b2f4434116f0886af165a
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/serialice/+/38501
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- qemu-0.15.x/build.sh
+++ qemu-0.15.x/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ./configure --disable-kvm --disable-sdl --enable-serialice \
-	    --target-list="x86_64-softmmu, i386-softmmu"
+	    --target-list="x86_64-softmmu, i386-softmmu" \
+	    --python=$(which python2) --disable-vnc-tls
 
 if [ $? -eq 0 ]; then
    make
